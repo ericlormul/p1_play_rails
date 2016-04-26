d31730779cd62ef0eb3eef1b1382322908648523
Eric Zhang <eric.zhang@frontdeskhq.com>
2016-04-26 10:26:21 -0700

    camp service use .json format and refine the comment make by eric

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  # comment out for heroku by Eric
+  # comment out for heroku by Eric, they have already been compressed
   # config.assets.js_compressor = :uglifier 
   # config.assets.css_compressor = :sass
 
